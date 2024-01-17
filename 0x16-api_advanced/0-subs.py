46d03e74b7300244b9313b04b6701db909ecbe80
iradukundapaci <paifique@gmail.com>
2024-01-17 17:36:12 +0200

    adding files

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
-"""
- function that return number of
-    subscribers for a sub reddit
-"""
-import requests as re
+'''A module containing functions for working with the Reddit API.
+'''
+import requests
+
+
+BASE_URL = 'https://www.reddit.com'
+'''Reddit's base API URL.
+'''
 
 
 def number_of_subscribers(subreddit):
-    """
-        function to get subscribers for a subreddit
-        subreddit: the provided sub reddit
-    """
-
-    user_agent = {'User-agent': 'Google Chrome Version 120.0.0.0'}
-
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    res = re.get(url, headers=user_agent)
-
-    if (res.ok):
-        content = res.json()
-        try:
-            return content.get("data").get("subscribers")
-        except (Exception):
-            return 0
-
-    else:
-        return 0
+    '''Retrieves the number of subscribers in a given subreddit.
+    '''
+    api_headers = {
+        'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Chrome/97.0.4692.71',
+            'Safari/537.36',
+            'Edg/97.0.1072.62'
+        ])
+    }
+    res = requests.get(
+        '{}/r/{}/about/.json'.format(BASE_URL, subreddit),
+        headers=api_headers,
+        allow_redirects=False
+    )
+    if res.status_code == 200:
+        return res.json()['data']['subscribers']
+    return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,33 +1,41 @@
 #!/usr/bin/python3
+'''A module containing functions for working with the Reddit API.
+'''
+import requests
 
-"""
-    Prints top 10 titles for given subreddit
-"""
 
-import requests as re
+BASE_URL = 'https://www.reddit.com'
+'''Reddit's base API URL.
+'''
 
 
 def top_ten(subreddit):
-    """
-        function to handle the printing
-        of top 10 title for a given subreddit
-    """
-
-    if subreddit is None or not isinstance(subreddit, str):
-        print("None")
-
-    user_agent = {'User-agent': 'Google Chrome Version 120.0.0.0'}
-    params = {'limit': 10}
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-
-    response = re.get(url, headers=user_agent, params=params)
-    results = response.json()
-
-    try:
-        result_data = results.get('data').get('children')
-
-        for i in result_data:
-            print(i.get('data').get('title'))
-
-    except Exception:
-        print("None")
+    '''Retrieves the title of the top ten posts from a given subreddit.
+    '''
+    api_headers = {
+        'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Chrome/97.0.4692.71',
+            'Safari/537.36',
+            'Edg/97.0.1072.62'
+        ])
+    }
+    sort = 'top'
+    limit = 10
+    res = requests.get(
+        '{}/r/{}/.json?sort={}&limit={}'.format(
+            BASE_URL,
+            subreddit,
+            sort,
+            limit
+        ),
+        headers=api_headers,
+        allow_redirects=False
+    )
+    if res.status_code == 200:
+        for post in res.json()['data']['children'][0:10]:
+            print(post['data']['title'])
+    else:
+        print(None)
